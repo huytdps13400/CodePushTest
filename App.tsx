4e66f2abcbf7bc74e4848cbe26f6c022bc265c77
Archer <>
2023-02-23 12:28:26 +0700

    🔥 remove code

--- App.tsx
+++ App.tsx
@@ -105,7 +105,7 @@ const App = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Read the docs to discover what to do next: 2
           </Section>
           <LearnMoreLinks />
         </View>
